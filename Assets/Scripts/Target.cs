51e76738c6e3b76d495ff03bfd258f550dd9d146
Guillaume ROBERT <gr99@orange.fr>
2021-05-10 12:08:38 +0200

    Modifs anims Guillaume

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Target : MonoBehaviour
+{
+    public GameObject Cible;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<NavMeshAgent>().destination = Cible.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
